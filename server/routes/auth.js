7d70f4f97f6ae2ef0ca353a13e2ce446d019d9c9
Rubel khan <rubel.pust53@gmail.com>
2026-05-11 11:17:14 +0600

    added apdate more

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { login, getMe, seedAdmin } = require('../controllers/authController');
-const { protect } = require('../middleware/auth');
-
 const { login, getMe, seedAdmin, debugLogin } = require('../controllers/authController');
-router.get('/debug', debugLogin);
+const { protect } = require('../middleware/auth');
 
 router.post('/login', login);
 router.get('/me', protect, getMe);
-router.post('/seed', seedAdmin); // Only for initial setup
 router.get('/seed', seedAdmin);
+router.get('/debug', debugLogin);
+
 module.exports = router;
